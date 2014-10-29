8f996f7cc540ed3ffe6bfdc95be283462e557819
elleryq <elleryq@gmail.com>
2014-10-29 11:18:58 +0800

    Fix wsgi issues.

--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,9 @@
 #!/usr/bin/env python
 import os
-
-
-#def execfile(afile, globalz=None, localz=None):
-#    with open(afile, "r") as fh:
-#        exec(fh.read(), globalz, localz)
-#
-#virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-#virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-#try:
-#    execfile(virtualenv, dict(__file__=virtualenv))
-#except IOError:
-#    pass
-
-
 from flask import Flask
 
 
-app = Flask(__name__)
+app = application = Flask(__name__)
 
 
 # The '/' page is accessible to anyone
