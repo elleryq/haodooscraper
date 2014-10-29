04a1b769ac539a5dd9e1e822e9bb7a9772bb2192
elleryq <elleryq@gmail.com>
2014-10-29 10:59:52 +0800

    Update requirements.

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
 
-setup(name='HaodooScraper',
+setup(name='HaodooScraper-Flask',
       version='1.0',
       description='HaodooScraper - Scraper and Web App',
       author='elleryq',
       author_email='elleryq@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Flask',
-                        'Flask-Bootstrap',
                         'requests',
                         'SQLAlchemy',
                         'lxml',
