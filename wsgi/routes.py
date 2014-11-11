61d43f175d9ad9e1f1dc525fda16a544b1365bd5
elleryq <elleryq@gmail.com>
2014-11-11 15:33:55 +0800

    Use model directly.

--- wsgi/routes.py
+++ wsgi/routes.py
@@ -1,23 +1,16 @@
 # -*- coding: utf-8 -*-
 import os
 from flask import Flask, Response, url_for
+from haodooscraper.model import Volume
+
 
 instancepath = os.path.join(os.environ['OPENSHIFT_PYTHON_DIR'], 'instance')
 app = Flask(__name__, instance_path=instancepath)
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
-db_url = os.environ.get('OPENSHIFT_MYSQL_DB_URL', None)
-if db_url:
-    db_url = db_url.replace("mysql", "mysql+mysqldb")
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
-    print(db_url)
-else:
-    print("OPENSHIFT_MYSQL_DB_URL not found.")
-
-db = SQLAlchemy(app)
-
 
 # The '/' page is accessible to anyone
 @app.route('/')
 def home():
-    return "Hello world!"
+    volumes = Volume.query_all()
+    return "Hello world! {0}".format(len(volumes))
