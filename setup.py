71b19a76ae996d2d68e194f6c442c312053d9b7e
elleryq <elleryq@gmail.com>
2014-11-11 11:19:44 +0800

    Revert to use setuptools.

--- setup.py
+++ setup.py
@@ -1,29 +1,5 @@
 import os
-from distutils.core import setup
-
-
-def is_package(path):
-    """Does path contain packages"""
-    return (
-        os.path.isdir(path) and
-        os.path.isfile(os.path.join(path, '__init__.py'))
-    )
-
-
-def find_packages(path, base=""):
-    """ Find all packages in path """
-    packages = {}
-    for item in os.listdir(path):
-        dir_ = os.path.join(path, item)
-        if is_package(dir_):
-            if base:
-                module_name = "%(base)s.%(item)s" % vars()
-            else:
-                module_name = item
-            packages[module_name] = dir_
-            packages.update(find_packages(dir_, module_name))
-    return packages
-
+from setuptools import setup, find_packages
 
 required_packages = ['Flask',
                      'requests',
