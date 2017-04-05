a104b0a51ebde2f163ef169d2720cb4f38a63233
Yan-ren Tsai <elleryq@gmail.com>
2017-04-05 22:32:58 +0800

    Implement search API
    * Also add backslash for endpoints

--- wsgi/routes.py
+++ wsgi/routes.py
@@ -17,8 +17,8 @@ def url_for_other_page(page):
 
 
 PAGE_SIZE = 10
-instancepath = os.path.join(os.environ['OPENSHIFT_PYTHON_DIR'], 'instance')
-app = Flask(__name__, instance_path=instancepath)
+instance_path = os.path.join(os.environ['OPENSHIFT_PYTHON_DIR'], 'instance')
+app = Flask(__name__, instance_path=instance_path)
 app.json_encoder = DynamicJSONEncoder
 app.config['PROPAGATE_EXCEPTIONS'] = True
 if "DEBUG" in os.environ:
@@ -32,6 +32,7 @@ api_v1 = APIBlueprint('api_v1', __name__, url_prefix='/api/v1')
 
 
 def get_page():
+    """Get 'page' parameter."""
     page_str = request.args.get('page', '1')
     try:
         page = int(page_str)
@@ -43,6 +44,7 @@ def get_page():
 # The '/' page is accessible to anyone
 @app.route('/')
 def home():
+    """Home."""
     q = request.args.get('q', '')
     page = get_page()
     return render_template(
@@ -51,36 +53,30 @@ def home():
         pagination=Volume.query_as_pagination(q, page, PAGE_SIZE))
 
 
-@app.route('/json')
-@jsonapi
-def query():
-    q = request.args.get('q', None)
-    page = get_page()
-    return {'results': list(Volume.query_as_pagination(
-        q, page, PAGE_SIZE).items())}
-
-
-@app.route('/api')
+@app.route('/api/')
 def api():
+    """Display API uasge."""
     return render_template("api.html")
 
 
-@app.route('/debug')
+@app.route('/debug/')
 @jsonapi
 def test_encoding():
+    """Test encoding."""
     page = 1
     q = None
-    results = Volume.query_as_pagination(q, page, PAGE_SIZE).items()
-    return {'results': list(results)}
+    result = Volume.query_as_pagination(q, page, PAGE_SIZE).items()
+    return {'result': list(result)}
 
 
-@api_v1.route('/book/<name>/')
-def api_book(name):
-    result = {
-        'name': name
-    }
-    return jsonify(result)
+@api_v1.route('/search/')
+def api_search():
+    """API - Search."""
+    q = request.args.get('q', None)
+    page = get_page()
+    return jsonify({'result': list(Volume.query_as_pagination(
+        q, page, PAGE_SIZE).items())})
 
 
-print(api_v1.routes_to_views_map)
+# print(api_v1.routes_to_views_map)
 app.register_blueprint(api_v1)
